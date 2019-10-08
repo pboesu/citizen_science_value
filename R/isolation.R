97bb28a4b8d65a70a5df6bd0e89dc0079d8b5bc6
LeaDambly <leadam@ceh.ac.uk>
2019-10-08 14:36:31 +0100

    added plot

--- R/isolation.R
+++ R/isolation.R
@@ -7,6 +7,7 @@ library(spdplyr)
 library(ggplot2)
 library(tidyr)
 library(scales)
+library(viridis)
 
 ebd <- read_tsv("C:/Users/leadam/Downloads/QE Hackathon/ebd_sampling_filtered_GB_2015_2018.txt")
 names(ebd) <- gsub(x = names(ebd),
@@ -20,7 +21,7 @@ ebd_wales <- ebd %>%
   unite("latlong", lat:long)
 
 
-isolation <- function(df = ebd_wales, mindate = "2015-01-01", maxdate = "2015-12-31"){
+isolation_func <- function(df = ebd_wales, mindate = "2015-01-01", maxdate = "2015-12-31"){
   
   mindate <- date(mindate)
   maxdate <- date(maxdate)
@@ -36,14 +37,20 @@ isolation <- function(df = ebd_wales, mindate = "2015-01-01", maxdate = "2015-12
 }
 
 # not pretty but works, aka I suck at loops, sorry
-iso_prio_2015 <- isolation(ebd_wales, mindate = "2015-01-01", maxdate = "2015-12-31") %>%
+iso_prio_2015 <- isolation_func(ebd_wales, mindate = "2015-01-01", maxdate = "2015-12-31") %>%
   mutate(year = 2015)
-iso_prio_2016 <- isolation(ebd_wales, mindate = "2016-01-01", maxdate = "2016-12-31") %>%
+iso_prio_2016 <- isolation_func(ebd_wales, mindate = "2016-01-01", maxdate = "2016-12-31") %>%
   mutate(year = 2016)
-iso_prio_2017 <- isolation(ebd_wales, mindate = "2017-01-01", maxdate = "2017-12-31") %>%
+iso_prio_2017 <- isolation_func(ebd_wales, mindate = "2017-01-01", maxdate = "2017-12-31") %>%
   mutate(year = 2017)
-iso_prio_2018 <- isolation(ebd_wales, mindate = "2018-01-01", maxdate = "2018-12-31") %>%
+iso_prio_2018 <- isolation_func(ebd_wales, mindate = "2018-01-01", maxdate = "2018-12-31") %>%
   mutate(year = 2018)
 
 iso_prio <- bind_rows(iso_prio_2015, iso_prio_2016, iso_prio_2017, iso_prio_2018) %>%
   dplyr::select(latlong, isolation, LATITUDE, LONGITUDE, year)
+
+save(iso_prio, file = "isolation.RData")
+
+ggplot(data = iso_prio) +
+  geom_point(aes(x = LONGITUDE, y = LATITUDE, colour = isolation)) +
+  scale_color_viridis(option = "plasma")
